935e98e55ec4e785a90fcf489bda75dbaf9557d5
benjaminnow <benscar3@gmail.com>
2019-06-30 19:58:28 -0400

    added an installation shell script

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+apt-get install git
+apt-get install python3-venv
+cd ~/Desktop
+git clone https://github.com/njcssa/summer2019_wombatgame.git
+cd summer2019_wombatgame
+source venv/bin/activate
+pip install -r requirements.txt
+
+
+arch=$(eval "uname -m")
+sixfourbit="x86_64"
+threetwobit="i686"
+if [ "$arch" = "$sixfourbit" ]
+then
+    # installing vscode for 64bit systems
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+    sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    apt-get install apt-transport-https
+    apt-get update
+    apt-get install code
+elif [ "$arch" = "$threetwobit" ]
+then
+    # installing vscode for 32bit systems
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+    sh -c 'echo "deb [arch=i386] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    apt-get install apt-transport-https
+    apt-get update
+    apt-get install code
+fi
---
+++ wombat8.md
@@ -0,0 +1,31 @@
+# Day 8 (Wednesday 7/18)
+
+[Link to Slideshow](google.com)
+
+## Topics:
+
+- 5-10 minutes of going over homework
+- 10 minute review of what we did last class
+- problem(student): make program which creates custom sized rectangles given parameters
+- problem(student): make a program which creates custom sized triangles given parameters
+
+
+
+## Notes:
+
+problem(student): make program which creates custom sized rectangles given parameters
+
+![](/gifs/day8/create_rect_2x2.gif)
+
+- use setup 0
+- you need to use some if statments here to compare the current coordinate location to the next and walk in a direction based on the change
+- for example, if the x changes by positive 1, then the wombat must walk east
+- if the x changes by negative 1, then the wombat must walk west
+- if the y changes by positive 1, then the wombat must walk south
+- if the y changes by negative 1, then the wombat must walk north
+
+
+
+## Homework:
+
+Continue on classwork if it's not finished. Otherwise, no homework. You should spend at least an hour on the homework and you are welcome to work longer than that. Be prepared to tell your teachers what progress you made at home.
