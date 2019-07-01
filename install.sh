354470b8fa3823399e30cb7da9807a068814e779
benjaminnow <benscar3@gmail.com>
2019-06-30 21:21:36 -0400

    added checks for other OS

--- install.sh
+++ install.sh
@@ -1,35 +1,63 @@
 #!/bin/sh
-apt-get update
-apt-get install git
-apt-get install python3-venv
-apt-get install python3-pip
-apt-get install curl
 
-cd ~/Desktop
-git clone https://github.com/njcssa/summer2019_wombatgame.git
-cd summer2019_wombatgame
-python3 -m venv venv
-. ./venv/bin/activate
-pip install -r requirements.txt
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo ${machine}
 
-
-arch=$(eval "uname -m")
-sixfourbit="x86_64"
-threetwobit="i686"
-if [ "$arch" = "$sixfourbit" ]
+if [ $machine = "Linux" ]
 then
-    # installing vscode for 64bit systems
-    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-    install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-    sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-    apt-get install apt-transport-https
+    # linux case (lubuntu)
     apt-get update
-    apt-get install code
-elif [ "$arch" = "$threetwobit" ]
+    apt-get install git
+    apt-get install python3-venv
+    apt-get install python3-pip
+    apt-get install curl
+
+    cd ~/Desktop
+    git clone https://github.com/njcssa/summer2019_wombatgame.git
+    cd summer2019_wombatgame
+    python3 -m venv venv
+    . ./venv/bin/activate
+    pip install -r requirements.txt
+
+
+    arch=$(eval "uname -m")
+    sixfourbit="x86_64"
+    threetwobit="i686"
+    if [ "$arch" = "$sixfourbit" ]
+    then
+        # installing vscode for 64bit systems
+        curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+        install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+        sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+        apt-get install apt-transport-https
+        apt-get update
+        apt-get install code
+    elif [ "$arch" = "$threetwobit" ]
+    then
+        # installing vscode for 32bit systems
+        curl -o code.deb -L https://go.microsoft.com/fwlink/?LinkID=760680
+        apt-get update
+        dpkg -i code.deb
+    fi
+elif [ $machine = "Mac" ]
 then
-    # installing vscode for 32bit systems
-    curl -o code.deb -L https://go.microsoft.com/fwlink/?LinkID=760680
-    apt-get update
-    dpkg -i code.deb
-    #apt-get install code
+    # macos case
+
+elif [ $machine = "MinGw"]
+then
+    # windows case
+    if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under 32 bits Windows NT platform
+    
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Do something under 64 bits Windows NT platform
+
+
 fi
